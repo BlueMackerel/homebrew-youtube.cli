555c94ec5ba9dfbd42d9c069d8fd0e92a5b9f843
K <mango1472@outlook.kr>
2025-05-31 19:47:21 +0900

    Add Homebrew

--- Formula/youtube.cli.rb
+++ Formula/youtube.cli.rb
@@ -1,7 +1,7 @@
 class YoutubeCli < Formula
   desc "Terminal YouTube player with ASCII video"
   homepage "https://github.com/BlueMackerel/youtube.cli"
-  url "https://github.com/BlueMackerel/homebrew-youtube.cli/archive/v1.0.0.tar.gz"
+  url "https://github.com/BlueMackerel/homebrew-youtube.cli/youtube.cli/v1.0.0.tar.gz"
   sha256 "19d7330608d58a83afdc18e529a16baf5333ff2b578b48d34279a8667d961c26"
   license "MIT"
 
