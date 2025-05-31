503b5135dd7dd88016341d00051821d5306aa34e
K <mango1472@outlook.kr>
2025-05-31 19:43:01 +0900

    Add Hoomebrew

---
+++ Formula/youtube.cli.rb
@@ -0,0 +1,19 @@
+class Mytool < Formula
+  desc "Terminal YouTube player with ASCII video"
+  homepage "https://github.com/BlueMackerel/youtube.cli"
+  url "https://github.com/BlueMackerel/homebrew-youtube.cli/archive/v1.0.0.tar.gz"
+  sha256 "19d7330608d58a83afdc18e529a16baf5333ff2b578b48d34279a8667d961c26"
+  license "MIT"
+
+  depends_on "python@3.12"
+  depends_on "ffmpeg"
+
+  def install
+    bin.install  "youtube-cli"
+  end
+
+  test do
+    system "#{bin}/youtube-cli"
+  end
+end
+
